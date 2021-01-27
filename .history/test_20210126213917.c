af2b0db63b84c66bc91f1d1701f09263598cb0d2
V1me <z1991998920@gmail.com>
2021-01-27 05:59:44 -0800

    edit gitignore

--- .history/test_20210126213917.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    system("cat Makefile; cat Makefile");
-    return 0;
-}
--- .history/test_20210126214047.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    system("cd ../piehook; make clean; make");
-    return 0;
-}
